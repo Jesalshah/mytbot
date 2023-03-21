a7ad538b2c832256dbf441880a3b6334e58742e4
Jesal Shah <jesal.shah.007@gmail.com>
2023-03-21 21:45:18 +0530

    Add files via upload

---
+++ stt.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from langchain import OpenAI
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+import openai
+import sys
+import streamlit as st
+import os
+
+
+# In[ ]:
+
+
+os.environ["OPENAI_API_KEY"] = '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'
+
+
+# In[ ]:
+
+
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+#st.sidebar.header('Training Bot')
+st.header("Welcome to Training World")
+
+
+# In[ ]:
+
+
+import streamlit as st
+_emp = st.empty()
+text = _emp.text_input("Enter Something here! ")
+if text:
+    index = GPTSimpleVectorIndex.load_from_disk('D://Trainingbot//index.json')
+    response = index.query(text, response_mode="compact")
+    st.write(response.response)
+
